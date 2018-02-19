9fb377c6bb208e11a90c9dea9a31bea66e398c91
Gunya <gunyadius@gmail.com>
2018-02-19 14:05:20 +0200

    FirstChapterLab
    FirstTaskLab

---
+++ FirstChapterLab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstChapterLab
+{
+    class MainClass
+    {
+        static Int64 Factorial(int x)
+        {
+            Int64 z = 1;
+            for (int i = 1; i <= x; i++)
+                z *= i;
+            return z;
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your value: ");
+            string arg=Console.ReadLine();
+            Int64 result = Factorial(int.Parse(arg));
+            Console.Write("Your factorial is: ");
+            Console.WriteLine(result);
+            Console.Beep(3000,3);
+            Console.ReadLine();
+        }
+    }
+}
